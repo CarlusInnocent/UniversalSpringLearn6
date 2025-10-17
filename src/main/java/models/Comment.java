3b1de519d0a9dfd2f49f533e23dbc5051291bbb3
CarlusInnocent <carlusinnocentainembabazi@gmail.com>
2025-10-17 23:48:48 +0300

    First Commit 2nd trial

--- src/main/java/models/Comment.java
+++ src/main/java/models/Comment.java
@@ -1,4 +1,20 @@
 package models;
 
 public class Comment {
+    private String text;
+    private String author;
+
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getText() {
+        return text;
+    }
+    public void setText(String text) {
+        this.text = text;
+    }
 }
