0b1057c4e5e84876bda89f009b43636086c12005
Carlus innocent Ainembabazi <carlusinnocentainembabazi@gmail.com>
2025-10-17 23:58:41 +0300

    sync 2 pcs

---
+++ src/main/java/Main.java
@@ -0,0 +1,18 @@
+import config.ProjectConfig;
+import model.Comment;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import services.CommentService;
+
+public class Main {
+    public static void main(String[] args) {
+        var c = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        var service = c.getBean(CommentService.class);
+
+        Comment comment = new Comment();
+        comment.setAuthor("Carlos");
+        comment.setText("Demo Test");
+
+        service.publishComment(comment);
+    }
+}
---
+++ src/main/java/model/Comment.java
@@ -0,0 +1,22 @@
+package model;
+
+public class Comment {
+    private String text;
+    private String author;
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String setAuthor() {
+        return author;
+    }
+}
